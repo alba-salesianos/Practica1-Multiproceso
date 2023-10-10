7614c49ed4ba659b09226062fbcd77f9ed8c90d8
alba-salesianos <usr3596@salesianos-lacuesta.net>
2023-10-10 19:37:08 +0100

    feat: created assistant static class to read from file

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,23 @@
+import net.salesianos.utils.Utils;
+
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
+
+        String[] vowelArray = {"a","e", "i", "o", "u"};
+        ArrayList<Process> allProcesses = new ArrayList<>();
+
+        String text = Utils.getText("src/loremipsum.txt").toLowerCase();
+
+        System.out.println(text);
+
+
+
+
+
+
+
 
     }
 }
---
+++ src/net/salesianos/utils/Utils.java
@@ -0,0 +1,33 @@
+package net.salesianos.utils;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class Utils {
+    public static String getText(String fileRoute) {
+        String text = null;
+        StringBuilder stringBuilder = new StringBuilder();
+
+        try {
+            BufferedReader bufferedReader = new BufferedReader(new FileReader(fileRoute));
+
+
+            String ls = System.getProperty("line.separator");
+
+            while ((text = bufferedReader.readLine()) != null) {
+                stringBuilder.append(text);
+                stringBuilder.append(ls);
+            }
+
+            stringBuilder.deleteCharAt(stringBuilder.length() - 1);
+
+            bufferedReader.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return stringBuilder.toString();
+    }
+
+}
